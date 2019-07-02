9338ae686429201133868fb7e3742a0aabb4b476
Da Liu <da.liu@ericsson.com>
2019-07-02 16:04:50 +0800

    Example fo class inheritance

--- class/class4.py
+++ class/class4.py
@@ -6,8 +6,11 @@ class Myclass4:
 class Submyclass4(Myclass4):
     pass
 
-
 submyclass4_1 = Submyclass4(10)
 print(submyclass4_1.name)
 print(submyclass4_1.age)
+print(isinstance(submyclass4_1,Myclass4))
+print(issubclass(Submyclass4,Myclass4))
+
+
 
