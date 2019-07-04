0c49f085da34effcaa2856f9eeaa63ae492ab205
Da Liu <da.liu@ericsson.com>
2019-07-04 15:10:00 +0800

    reorgnize struture of project and edit README.md

--- builtin/enum-1.py
+++ builtin/enum-1.py
@@ -3,7 +3,6 @@ class Color(Enum): #Even though we use the class syntax to create Enums, Enums a
     RED = 1
     GREEN = 2
     BLUE = 3
-
 #Color.RED.name = 'ORANGE'  # This action will raise exeption
 myenum = Color # Make a copy of Color enum object
 print(myenum.RED.value)
